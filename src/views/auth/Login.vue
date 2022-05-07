c2bd6e85bd44d12cff797bb0868ced1247118346
denkoksz <denkoksz@gmail.com>
2022-05-07 15:05:05 +0200

    add pending state at composables

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -4,7 +4,7 @@
     <input type="email" placeholder="Email" v-model="email">
     <input type="password" placeholder="Password" v-model="password">
     <div v-if="error" class="error">{{error}}</div>
-    <button>Log in</button>
+    <button v-if="!isPending">Log in</button>
   </form>
 </template>
 
@@ -14,7 +14,7 @@ import {ref} from "vue";
 
 export default {
   setup() {
-    const {error, login} = useLogin();
+    const {error, login, isPending } = useLogin();
 
     const email = ref('');
     const password = ref('');
@@ -26,7 +26,7 @@ export default {
       }
     };
 
-    return { email, password, handleSubmit, error };
+    return { email, password, handleSubmit, error, isPending };
   }
 }
 </script>
