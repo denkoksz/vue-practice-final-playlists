5697bdbc6e552a9978ddf2831ad1aacf32f52d2b
denkoksz <denkoksz@gmail.com>
2022-05-08 21:42:04 +0200

    add playlist document to storage

--- src/composables/useStorage.js
+++ src/composables/useStorage.js
@@ -15,7 +15,7 @@ const useStorage = () => {
 
         try {
             const response = await storageRef.put(file);
-            url.value = response.ref.getDownloadURL();
+            url.value = await response.ref.getDownloadURL();
         } catch (err) {
             error.value = err.message
         }
