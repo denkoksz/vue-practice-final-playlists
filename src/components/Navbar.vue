aef5e8cf80a086f96fc841f36bf87872fa9df6df
denkoksz <denkoksz@gmail.com>
2022-05-31 13:11:38 +0200

    display currently logged in user's name

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,7 @@
         <div v-if="user">
           <router-link :to="{ name: 'CreatePlaylist'}">Create Playlist</router-link>
           <router-link :to="{ name: 'UserPlaylists'}">My Playlist</router-link>
+          <span>Hi there, {{ user.displayName}}</span>
           <button @click="handleLogout">Logout</button>
         </div>
         <div v-else>
@@ -66,5 +67,12 @@ export default {
   }
   nav img {
     max-height: 66px;
+  }
+  span {
+    font-size: 14px;
+    display: inline-block;
+    margin-left: 16px;
+    padding-left: 16px;
+    border-left: 1px solid #eee;
   }
 </style>
